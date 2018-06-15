204a51005d498102fb8d7b28f99a309b4c13f533
surgelt <surge_lt@163.com>
2018-06-15 17:39:20 +0800

    first

---
+++ spring-cloud3/spring-cloud3-service-feign/src/main/java/org/spring/cloud3/service/feign/service/SchedualServiceHiHystric.java
@@ -0,0 +1,11 @@
+package org.spring.cloud3.service.feign.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
